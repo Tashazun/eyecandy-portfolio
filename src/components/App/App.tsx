f990d63822527ecc6b9be42637e1180a3c5abebd
Tasha R Zuniga <Tashazun@gmail.com>
2019-02-08 17:22:13 -0800

    setup for paralax scroll effect

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -7,15 +7,30 @@ import Contact from '../Contact/Contact';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App-container">
         <header className="App-header">
           <p>
             Personal Website
           </p>
         </header>
         <section>
+          <div id="image1">
+            <p>
+              About
+            </p>
+          </div>
           <About />
+          <div id="iamge2">
+            <p>
+              Projects
+            </p>
+          </div>
           <Projects />
+          <div id="image3">
+            <p>
+              Contact
+            </p>
+          </div>
           <Contact />
         </section>
         <footer className="App-footer">
