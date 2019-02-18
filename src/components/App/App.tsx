560b247a64f3a57d59c480de4293fedc425b3d11
Tasha R Zuniga <Tashazun@gmail.com>
2019-02-18 14:40:01 -0800

    shutter effect mostly functional on landing page

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,16 +4,23 @@ import About from '../About/About';
 import Projects from '../Projects/Projects';
 import Contact from '../Contact/Contact';
 
-import picture from '../../assets/beach-pic.jpg';
+
 class App extends Component {
   render() {
     return (
       <div className="App-container">
         <section className="App-landing-page">
-          <img src={picture} alt='Photo of Tasha Zuniga gazing out to sea' />
-          <article>
-            <h3>Tasha Zuniga</h3>
-            <h4>Full-Stack Javascript Developer</h4>
+          <article id='bottom-shutter'>
+            <h4 className='inner-text'>Cinephile +</h4>
+            <h4 className='inner-text'>Artist +</h4> 
+            <h4 className='inner-text'>Cakes +</h4>
+            <h4 className='inner-text'>+ Great Ideas</h4>
+            <div id='top-shutter'>
+              <h4 className='outer-text'><p>Tasha Zuniga.</p></h4>
+              <h4 className='outer-text'><p>Full-Stack</p></h4>
+              <h4 className='outer-text'><p>Javascript</p></h4>
+              <h4 className='outer-text'><p>Developer</p></h4>
+            </div>
           </article>
         </section>
         <section>
