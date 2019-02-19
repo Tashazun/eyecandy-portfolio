d44f8155760ee99b5c1263e2fba2fc5f928badf9
Tasha R Zuniga <Tashazun@gmail.com>
2019-02-19 14:14:02 -0800

    starting structure for projects component

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -6,6 +6,11 @@ class Projects extends Component {
         return (
             <div id='projects-container'>
                 <h2>Projects</h2>
+                <div id='projects-list'>
+                    <div className='project-block'>Project1</div>
+                    <div className='project-block'>Project2</div>
+                    <div className='project-block'>Project3</div>
+                </div>
             </div>
         );
     }
