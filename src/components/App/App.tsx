25f4c2db15951f06fd7858ce724361c976dcfeed
Tasha R Zuniga <Tashazun@gmail.com>
2019-02-18 16:28:32 -0800

    changing App header to a seperate component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -16,10 +16,10 @@ class App extends Component {
             <h4 className='inner-text'>+ Great Ideas</h4>
             <h4 className='inner-text'>+ Oregonian</h4>
             <div id='top-shutter'>
-              <h4 className='outer-text'><p>Tasha Zuniga.</p></h4>
-              <h4 className='outer-text'><p>Full-Stack</p></h4>
-              <h4 className='outer-text'><p>Javascript</p></h4>
-              <h4 className='outer-text'><p>Developer</p></h4>
+              <h4 className='outer-text block-2'><p>Tasha Zuniga.</p></h4>
+              <h4 className='outer-text block-3'><p>Full-Stack</p></h4>
+              <h4 className='outer-text block-4'><p>Javascript</p></h4>
+              <h4 className='outer-text block-5'><p>Developer.</p></h4>
               <h4 className='outer-text filler-block'></h4>
             </div>
           </article>
