82997bf7345a0d84d5b20b500ef781850a2bbeb7
Tasha R Zuniga <Tashazun@gmail.com>
2019-03-05 13:12:27 -0800

    adding vivus to animate svgs, functional with a few unintended side effects

--- cypress/integration/app_spec.js
+++ cypress/integration/app_spec.js
@@ -1,7 +1,11 @@
 describe("App", () => {
-  it("renders all components", () => {
-    cy.visit("localhost:3000");
+  beforeEach(() => {
+    cy.vist("/");
+  });
 
+  it("renders all components", () => {
     cy.get("#image1").should("be.visible");
+    cy.get("#image2").should("be.visible");
+    cy.get("#image3").should("be.visible");
   });
 });
--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,26 +1,42 @@
-import React, { Component } from 'react';
-import './Landing.scss';
-
-import { FaBirthdayCake } from 'react-icons/fa';
+import React, { Component } from "react";
+import "./Landing.scss";
+import Poppies from "../../assets/PoppiesSimpleWhite.svg";
+import { FaBirthdayCake } from "react-icons/fa";
+import Vivus from "vivus";
 
 class Landing extends Component {
-    render() {
-        return (
-            <div className='landing-page'>
-              <article id='bottom-shutter'>
-                <h4 className='inner-text block-1'>Artist +</h4> 
-                <h4 className='inner-text block-2'>Cake <FaBirthdayCake /> +</h4>
-                <h4 className='inner-text block-3'>+ Great Ideas</h4>
-                <div id='top-shutter'>
-                  <h4 className='outer-text block-4'><p>Tasha Zuniga.</p></h4>
-                  <h4 className='outer-text block-5'><p>Full-Stack</p></h4>
-                  <h4 className='outer-text block-6'><p>Javascript</p></h4>
-                  <h4 className='outer-text block-7'><p>Developer.</p></h4>
-                </div>
-              </article>
-            </div>
-        );
-    }
+  componentDidMount() {
+    new Vivus("poppies", { type: "oneByOne", duration: 15000, file: Poppies });
+  }
+
+  render() {
+    return (
+      <div className="landing-page">
+        <article id="bottom-shutter">
+          <h4 className="inner-text block-1">Artist +</h4>
+          <h4 className="inner-text block-2">
+            Cake <FaBirthdayCake /> +
+          </h4>
+          <h4 className="inner-text block-3">+ Great Ideas</h4>
+          <div id="top-shutter">
+            <h4 className="outer-text block-4">
+              <p>Tasha Zuniga.</p>
+            </h4>
+            <h4 className="outer-text block-5">
+              <p>Full-Stack</p>
+            </h4>
+            <h4 className="outer-text block-6">
+              <p>Javascript</p>
+            </h4>
+            <h4 className="outer-text block-7">
+              <p>Developer.</p>
+            </h4>
+          </div>
+        </article>
+        <div id="poppies" />
+      </div>
+    );
+  }
 }
 
 export default Landing;
