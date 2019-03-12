ad472359af331f07165c0dae85faf0aa0c8b14e0
Tasha R Zuniga <Tashazun@gmail.com>
2019-03-11 21:07:43 -0700

    adding assets

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import "./Landing.scss";
 import Poppies from "../../assets/PoppiesSimpleWhite.svg";
-import { FaBirthdayCake } from "react-icons/fa";
+import PoppiesBlack from "../../assets/PoppiesSimpleBlack.svg";
+import Logo from "../../assets/DesignLogo.svg";
 import Vivus from "vivus";
 
 class Landing extends Component {
@@ -12,32 +13,22 @@ class Landing extends Component {
       duration: 45000,
       file: Poppies
     });
+
+    new Vivus("top", {
+      type: "oneByOne",
+      start: "autostart",
+      duration: 45000,
+      file: PoppiesBlack
+    });
   }
 
   render() {
     return (
       <div className="landing-page">
-        <article id="bottom-shutter">
-          <h4 className="inner-text block-1">Artist +</h4>
-          <h4 className="inner-text block-2">
-            Cake <FaBirthdayCake /> +
-          </h4>
-          <h4 className="inner-text block-3">+ Great Ideas</h4>
-          <div id="top-shutter">
-            <h4 className="outer-text block-4">
-              <p>Tasha Zuniga.</p>
-            </h4>
-            <h4 className="outer-text block-5">
-              <p>Full-Stack</p>
-            </h4>
-            <h4 className="outer-text block-6">
-              <p>Javascript</p>
-            </h4>
-            <h4 className="outer-text block-7">
-              <p>Developer.</p>
-            </h4>
-          </div>
-        </article>
+        <div id="top">
+          <img src={Logo} alt="Mayan glyph that spells Zuniga" />
+          <h2>Tasha Zuniga</h2>
+        </div>
         <div id="poppies" />
       </div>
     );
