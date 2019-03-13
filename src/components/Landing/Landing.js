4dc516a7d934161d14a7a136aa521a87a0064911
Tasha R Zuniga <Tashazun@gmail.com>
2019-03-12 22:04:07 -0700

    final layout for landing page

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,20 +1,23 @@
 import React, { Component } from "react";
+import Vivus from "vivus";
+
 import "./Landing.scss";
+
 import Poppies from "../../assets/PoppiesSimpleWhite.svg";
 import PoppiesBlack from "../../assets/PoppiesSimpleBlack.svg";
 import Logo from "../../assets/DesignLogo.svg";
-import Vivus from "vivus";
+import { FaArrowDown } from "react-icons/fa";
 
 class Landing extends Component {
   componentDidMount() {
-    new Vivus("poppies", {
+    new Vivus("white", {
       type: "oneByOne",
       start: "autostart",
       duration: 45000,
       file: Poppies
     });
 
-    new Vivus("top", {
+    new Vivus("black", {
       type: "oneByOne",
       start: "autostart",
       duration: 45000,
@@ -25,11 +28,18 @@ class Landing extends Component {
   render() {
     return (
       <div className="landing-page">
-        <div id="top">
+        <div id="top-layer">
           <img src={Logo} alt="Mayan glyph that spells Zuniga" />
           <h2>Tasha Zuniga</h2>
+          <div id="black" />
         </div>
-        <div id="poppies" />
+        <div id="bottom-layer">
+          <div>
+            <h3>Hi</h3>
+            <FaArrowDown />
+          </div>
+          <div id="white" />
+        </div>
       </div>
     );
   }
