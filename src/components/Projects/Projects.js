19ffef4850d1ebc8feef5e818ad60d08ff5e1c11
Tasha R Zuniga <Tashazun@gmail.com>
2019-03-18 11:47:57 -0700

    setting up assets and links on project list

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -6,10 +6,10 @@ class Projects extends Component {
     return (
       <div id="projects-container">
         <h1>02 // Some projects she's worked on-</h1>
-        <div id="project-block1">Project1</div>
-        <div id="project-block2">Project2</div>
-        <div id="project-block3">Project3</div>
-        <div id="project-block4">Project4</div>
+        <div id="project-block1" />
+        <div id="project-block2" />
+        <div id="project-block3" />
+        <div id="project-block4" />
         <div id="works">
           <h3>Works</h3>
         </div>
