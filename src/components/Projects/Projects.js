57800be3a6ad56c256495647de61ea3626ee79e5
Tasha R Zuniga <Tashazun@gmail.com>
2019-03-24 14:15:08 -0700

    adding partial scss files for more modular behavior

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,21 +5,36 @@ class Projects extends Component {
   render() {
     return (
       <div id="projects-container">
-        <h1>02 // Some projects she's worked on-</h1>{" "}
-        {/* eslint-disable-line */}
+        <h1>02 // Some projects</h1>
         <a id="project-link1" href="https://mobile-eats.herokuapp.com/">
+          <div className="project-title">
+            <h3>Food Truck Tracker</h3>
+            <h4>Design & Development</h4>
+          </div>
           <div className="project-block" />
         </a>
         <a
           id="project-link2"
           href="https://play.google.com/store/apps/details?id=com.alchemycodelab.mercury.app"
         >
+          <div className="project-title">
+            <h3>Ugyde Review App</h3>
+            <h4>Design & Development</h4>
+          </div>
           <div className="project-block" />
         </a>
         <a id="project-link3" href="https://tashazun.github.io/Cute-Xylophone/">
+          <div className="project-title">
+            <h3>Zen Xylophone</h3>
+            <h4>Design & Development</h4>
+          </div>
           <div className="project-block" />
         </a>
         <a id="project-link4" href="https://deepfocus.herokuapp.com">
+          <div className="project-title">
+            <h3>Technical Film Reviews</h3>
+            <h4>Design & Development</h4>
+          </div>
           <div className="project-block" />
         </a>
         <div id="works">
