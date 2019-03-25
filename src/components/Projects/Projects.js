e902207edc8cb6b10bbaaa1a7a9fcb2bd39f4119
Tasha R Zuniga <Tashazun@gmail.com>
2019-03-25 09:53:49 -0700

    styling layout of projects page

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -6,37 +6,42 @@ class Projects extends Component {
     return (
       <div id="projects-container">
         <h1>02 // Some projects</h1>
-        <a id="project-link1" href="https://mobile-eats.herokuapp.com/">
-          <div className="project-title">
-            <h3>Food Truck Tracker</h3>
-            <h4>Design & Development</h4>
-          </div>
-          <div className="project-block" />
-        </a>
-        <a
-          id="project-link2"
-          href="https://play.google.com/store/apps/details?id=com.alchemycodelab.mercury.app"
-        >
-          <div className="project-title">
-            <h3>Ugyde Review App</h3>
-            <h4>Design & Development</h4>
-          </div>
-          <div className="project-block" />
-        </a>
-        <a id="project-link3" href="https://tashazun.github.io/Cute-Xylophone/">
-          <div className="project-title">
-            <h3>Zen Xylophone</h3>
-            <h4>Design & Development</h4>
-          </div>
-          <div className="project-block" />
-        </a>
-        <a id="project-link4" href="https://deepfocus.herokuapp.com">
-          <div className="project-title">
-            <h3>Technical Film Reviews</h3>
-            <h4>Design & Development</h4>
-          </div>
-          <div className="project-block" />
-        </a>
+        <div id="project-frame">
+          <a id="project-link1" href="https://mobile-eats.herokuapp.com/">
+            <div className="project-title">
+              <h3>Food Truck Tracker</h3>
+              <h4>Design & Development</h4>
+            </div>
+            <div className="project-block" />
+          </a>
+          <a
+            id="project-link2"
+            href="https://play.google.com/store/apps/details?id=com.alchemycodelab.mercury.app"
+          >
+            <div className="project-title">
+              <h3>Ugyde Review App</h3>
+              <h4>Design & Development</h4>
+            </div>
+            <div className="project-block" />
+          </a>
+          <a
+            id="project-link3"
+            href="https://tashazun.github.io/Cute-Xylophone/"
+          >
+            <div className="project-title">
+              <h3>Zen Xylophone</h3>
+              <h4>Design & Development</h4>
+            </div>
+            <div className="project-block" />
+          </a>
+          <a id="project-link4" href="https://deepfocus.herokuapp.com">
+            <div className="project-title">
+              <h3>Technical Film Reviews</h3>
+              <h4>Design & Development</h4>
+            </div>
+            <div className="project-block" />
+          </a>
+        </div>
         <div id="works">
           <h3>Works</h3>
         </div>
--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,15 +1,5 @@
 import React, { Component } from "react";
 import doggo from "../../assets/doggo.svg";
-import {
-  FaSass,
-  FaDocker,
-  FaReact,
-  FaVuejs,
-  FaNodeJs,
-  FaNpm,
-  FaJsSquare,
-  FaGitSquare
-} from "react-icons/fa";
 import "./Skills.scss";
 
 class Skills extends Component {
@@ -18,34 +8,6 @@ class Skills extends Component {
       <div id="skills-container">
         <img src={doggo} alt="cute doggo" />
         <h1>03 \\ What can she do?</h1>
-        <div id="skills-front">
-          <h2>Front:</h2>
-          <div>
-            <FaJsSquare />
-            <FaSass />
-            <FaReact />
-            <FaVuejs />
-          </div>
-        </div>
-        <div id="skills-back">
-          <h2>Back:</h2>
-          <div>
-            <FaNodeJs />
-            <span className="devicons devicons-postgresql" />
-            <span className="devicons devicons-mongodb" />
-            <span className="devicons devicons-heroku" />
-            <span className="devicons devicons-illustrator" />
-          </div>
-        </div>
-        <div id="skills-mid">
-          <h2>Side-to-side:</h2>
-          <div>
-            <span className="devicons devicons-jira" />
-            <FaDocker />
-            <FaNpm />
-            <FaGitSquare />
-          </div>
-        </div>
         <div id="skills">
           <div id="arrow-container">
             <div id="arrow-tail" />
